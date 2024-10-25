305c4cb4d919623e1fa7b3e9e7b407b20f324aec
Pejro-coder <peter.stepanic@hotmail.com>
2024-10-25 22:55:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import json
 from twilio.rest import Client
 from dotenv import load_dotenv
 
+# THE TODAY's DATA IS AVAILIBLE AT 22:30 CET (usa stock close time) SO THE PROGRAM RUNS CORRECTLY AFTER 22:30 and before MIDNIGHT
+# THE TODAY's DATA IS AVAILIBLE AT 22:30 CET (usa stock close time) SO THE PROGRAM RUNS CORRECTLY AFTER 22:30 and before MIDNIGHT
+# THE TODAY's DATA IS AVAILIBLE AT 22:30 CET (usa stock close time) SO THE PROGRAM RUNS CORRECTLY AFTER 22:30 and before MIDNIGHT
+
 load_dotenv("C:/Users/peter/EnvironmentVariables/.env")
 
 STOCK = "TSLA"
@@ -43,35 +47,8 @@ data_daily = response_daily.json()
 print(data_daily)
 
 
-# ---------------------- Hourly ----------------------
-# parameters_hourly = {
-#     "function": "TIME_SERIES_INTRADAY",
-#     "interval": "60min",
-#     "symbol": STOCK,
-#     "apikey": STOCK_API_KEY,
-# }
-# response_hourly = requests.get(url="https://www.alphavantage.co/query", params=parameters_hourly)
-# response_hourly.raise_for_status()
-# data_hourly = response_hourly.json()
-# print(data_hourly)
-
-
-# # testing file, cause API requests are limited to 25 on free
-# with open("today_daily_update.json", "r") as file:
-#     data_daily = json.load(file)
-#     print(data_daily)
-
-# ---------------------------- Get yesterday's date, from today's date,
-# so it can be used to filter out yesterday's closing price in the json ----------------------------
-print()
-# today_date = dt.datetime.now().date()
-# year = today_date.year
-# month = today_date.month
-# day = today_date.day
-# yesterday = day - 1
-# yesterday_date = f"{year}-{month}-{yesterday}" #YESTERDAY's DATE
-yesterday_date = (dt.datetime.now() - dt.timedelta(days=1)).strftime('%Y-%m-%d')
 # ---------------------------- Yesterday's closing price & Today's opening price ----------------------------
+yesterday_date = (dt.datetime.now() - dt.timedelta(days=1)).strftime('%Y-%m-%d')
 today_date = f"{dt.datetime.now().date()}"
 
 # TODO 1.
@@ -107,35 +84,10 @@ else:
 
         print(message.body)
 
-
-    if price_ratio > 1.05:
+    # 1% change for testing, should be 5% or so.
+    if price_ratio > 1.01:
         text = f"{COMPANY_NAME}🚀 +{percentage_change}% \n{news_to_send}"
         twilio_send()
-    elif price_ratio < 0.95:
+    elif price_ratio < 0.99:
         text = f"{COMPANY_NAME}🔻{percentage_change}% \n{news_to_send}"
         twilio_send()
-
-
-
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number.
-
-
-#Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' 
-portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' 
-and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
